a710211e92b9e4cb34eaeca86890b13d64fa84bd
Johnathan James <haxwell@gmail.com>
2019-06-24 19:09:55 -0600

    Generated the returning-user page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'new-user', loadChildren: './new-user/new-user.module#NewUserPageModule' },
+  { path: 'returning-user', loadChildren: './returning-user/returning-user.module#ReturningUserPageModule' },
 ];
 
 @NgModule({
