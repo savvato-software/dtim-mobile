a50b319e14cf0dff1cda36bbb96879fc8753e6a8
Johnathan James <haxwell@gmail.com>
2019-06-24 19:09:12 -0600

    Generated the new-user page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'new-user', loadChildren: './new-user/new-user.module#NewUserPageModule' },
 ];
 
 @NgModule({
