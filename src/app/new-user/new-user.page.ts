69d12cbc59cec953e02a525177402860fe64dc10
Johnathan James <haxwell@gmail.com>
2019-07-07 11:22:06 -0600

    New user input form validates email and phone now

--- src/app/new-user/new-user.page.ts
+++ src/app/new-user/new-user.page.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { AlertService } from '../_services/alert.service';
 import { UserService } from '../_services/user.service';
+
+import { CountryPhone } from './country-phone.model';
+import { PhoneValidator } from '../validators/phone.validator';
 
 @Component({
   selector: 'app-new-user',
@@ -16,17 +20,58 @@ export class NewUserPage implements OnInit {
   	phone = undefined;
   	email = undefined;
 
+  	validations_form: FormGroup;  	
+  	country_phone_group: FormGroup;
+
+  	countries: Array<CountryPhone>;
+
     constructor(private _location: Location,
 			    private _router: Router,
 			    private _route: ActivatedRoute,
     			private _userService: UserService,
-    			private _alertService: AlertService ) {
+    			private _alertService: AlertService,
+    			private formBuilder: FormBuilder ) {
 
     }
 
+	validation_messages = {
+		'name': [
+		  { type: 'required', message: 'Name is required.' }
+		],
+		'email': [
+		  { type: 'required', message: 'Email is required.' },
+		  { type: 'pattern', message: 'Please enter a valid email.' }
+		],
+		'phone': [
+		  { type: 'required', message: 'Phone is required.' },
+		  { type: 'validCountryPhone', message: 'The phone should be ten digits long.' }
+		]
+	};
+
   	ngOnInit() {
-  		
-  	}
+	    this.countries = [
+	      new CountryPhone('US', 'United States')
+	    ];
+
+	    let country = new FormControl(this.countries[0], Validators.required);
+	    let phone = new FormControl('', Validators.compose([
+	      Validators.required,
+	      PhoneValidator.validCountryPhone(country)
+	    ]));
+	    this.country_phone_group = new FormGroup({
+	      country: country,
+	      phone: phone
+	    });
+
+		this.validations_form = this.formBuilder.group({
+		  name: new FormControl('', Validators.required),
+		  email: new FormControl('', Validators.compose([
+		    Validators.required,
+		    Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+		  ])),
+		  country_phone: this.country_phone_group
+		});
+	}
 
 	onNameChange($event) {
 		this.name = $event.currentTarget.value;
