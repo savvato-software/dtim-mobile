db5cda9c0a3ae7094ce3bbf7a1ba56dfeeb5cedc
Johnathan James <haxwell@gmail.com>
2019-08-25 02:38:19 -0600

    Beginning Cypress support!

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,13 @@
+context('logging in', () => {
+	it('succeeds on the happy path', () => {
+		
+	})
+
+	it('does not allow you to submit new acct info if the email is invalid', () => {
+		
+	})
+
+	it('does not allow you to submit new acct info if the phone is invalid', () => {
+		
+	})
+})
---
+++ cypress/integration/mock-interview-session-mgmt.spec.js
@@ -0,0 +1,13 @@
+context('create a mock interview session', () => {
+	it('starts with no session in progress', () => {
+		
+	})
+
+	it('shows an empty list for who\'s attending, because no one has yet signed in', () => {
+		
+	})
+
+	it('throws up an alert if you start a session and one is already in progress', () => {
+		
+	})
+})
