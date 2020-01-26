b05ba267b016d4060c4c56d874f3353d135a6b1b
Johnathan James <haxwell@gmail.com>
2020-01-26 07:33:13 -0700

    Rolling HEAD back to ed7d26

--- src/_environments/environment.dev.ts
+++ src/_environments/environment.dev.ts
@@ -1,3 +1,17 @@
+/*
+ * These are settings used in the application. They can be different depending on the environment they are run in.
+ * 
+ * For instance if you wanted a setting that only ran in our testing environment, you would put that in a file named
+ *  environment.staging.ts
+ * where the word staging indicates the environment this file is concerned with. Your other choices are 'dev' and 'prod',
+ * as those are the only three environments we have.
+ *
+ * When you want the application to point to another environment, you copy this file over the
+ *  environment.ts
+ * file. The system looks there for the actual current settings. So when you copy the environment specific settings there,
+ * those settings become the environmental settings that the app uses.
+ *
+ */
 
 export const domainInfo = {
   domain: 'localhost',
