3c27d37524f0b056fdcafb9476f9d6a24b6b7024
Brayden Copley <braydenc303@gmail.com>
2020-02-18 13:56:37 -0700

    Added some new classes specifically for testing purposes, and the first two tests

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,13 +1,95 @@
 context('logging in', () => {
 	it('succeeds on the happy path', () => {
-		
+		cy.visit('localhost:8100/returning-user')     
+
+		cy.get('[data-cy=phone-email]') 
+			.children('.native-input')
+  			.first()
+		  	.type('test@test.com')   /
+	
+		cy.get('ion-button')
+		  .contains('Find Me')     
+		  .click()                
+	
+		cy.get('.alert-title')              
+		  .should('contain', 'Found you!')
 	})
 
 	it('does not allow you to submit new acct info if the email is invalid', () => {
+		//The way I am going to write this test, It should fail if working properly because the submit button will be disabled.
+
+		cy.visit('localhost:8100/new-user')     
+
+		cy.get('[data-cy=name-input]') 
+			.children('.native-input')
+  			.first()
+		  	.type('Test')   /
+	
+		cy.get('[data-cy=email-input]')  
+			.children('.native-input')
+			.first()
+		  	.type('test')   
+	
+		cy.get('ion-button')
+			.contains('Sign Me Up!')
+			.should('have.attr', 'class', 'submit-btn md button button-solid button-disabled ion-activatable ion-focusable hydrated')     
+		  
+		cy.get('[data-cy=email-input]')  
+			.children('.native-input')
+			.first()
+			.type('@')
 		
+		cy.get('ion-button')
+			.contains('Sign Me Up!')
+			.should('have.attr', 'class', 'submit-btn md button button-solid button-disabled ion-activatable ion-focusable hydrated') 
+
+		cy.get('[data-cy=email-input]')  
+			.children('.native-input')
+			.first()
+			.type('gmail')
+		
+		cy.get('ion-button')
+			.contains('Sign Me Up!')
+			.should('have.attr', 'class', 'submit-btn md button button-solid button-disabled ion-activatable ion-focusable hydrated')
+		
+			cy.get('[data-cy=email-input]')  
+			.children('.native-input')
+			.first()
+			.type('.')
+		
+		cy.get('ion-button')
+			.contains('Sign Me Up!')
+			.should('have.attr', 'class', 'submit-btn md button button-solid button-disabled ion-activatable ion-focusable hydrated') 
+		
+			cy.get('[data-cy=email-input]')  
+			.children('.native-input')
+			.first()
+			.type('com')
+		
+		cy.get('ion-button')
+			.contains('Sign Me Up!')
+			.should('have.attr', 'class', 'submit-btn md button button-solid ion-activatable ion-focusable hydrated') 
+		// cy.get('.alert-title')              
+		//   .should('contain', 'You\'re in!')
 	})
 
 	it('does not allow you to submit new acct info if the phone is invalid', () => {
-		
+		// cy.visit('localhost:8100/new-user')     
+
+		// cy.get('[data-cy=name-input]') 
+		// 	.children('.native-input')
+  		// 	.first()
+		//   		.type('Test')   /
+	
+		// cy.get('[data-cy=email-input]')  
+		// .children('.native-input')
+		// .first()
+		//   .type('test@test.com')   
+	
+		// cy.get('[data-cy=submit]')     
+		//   .click()                
+	
+		// cy.get('.alert-title')              
+		//   .should('contain', 'You\'re in!')
 	})
 })
