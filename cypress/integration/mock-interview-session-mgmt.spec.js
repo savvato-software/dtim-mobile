0eeac2624791681315115a3690bbd4f321d66156
Johnathan James <haxwell@gmail.com>
2020-02-19 09:07:02 -0700

    Added two cypress tests to verify the warning when the session has/hasnt started

--- cypress/integration/mock-interview-session-mgmt.spec.js
+++ cypress/integration/mock-interview-session-mgmt.spec.js
@@ -1,6 +1,18 @@
 context('create a mock interview session', () => {
-	it('starts with no session in progress', () => {
-		
+	it('shows the "No Session Started" warning when the most recent session is more than three hours ago', () => {
+		cy.server();
+		cy.route("/api/mockinterviewsession/mostrecent", {id: 1, timestamp: 1581089268000}) // indicate session started long ago
+
+		cy.visit("/home");
+		cy.get('[data-test="home-admin-warning"]').should("be.visible");
+	})
+
+	it('doesnt show the "No Session Started" warning when the most recent session is less than three hours ago', () => {
+		cy.server();
+		cy.route("/api/mockinterviewsession/mostrecent", {id: 1, timestamp: new Date().getTime() - 120000 }) // indicate session started two minutes ago
+
+		cy.visit("/home");
+		cy.get('[data-test="home-admin-warning"]').should("not.be.visible");
 	})
 
 	it('shows an empty list for who\'s attending, because no one has yet signed in', () => {
