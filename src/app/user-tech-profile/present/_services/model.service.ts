c3e233b7cf5400693802d4e537ab6fc4e70cd26a
Johnathan James <haxwell@gmail.com>
2020-03-03 13:01:38 -0700

    Present modelService, implemented getPathsForCareerGoalQuestions(). It returns the paths in the career goal which have questions listed in the nextQuestions object. This is so that we only show what is immediate.

--- src/app/user-tech-profile/present/_services/model.service.ts
+++ src/app/user-tech-profile/present/_services/model.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+import * as jsonpath from 'jsonpath'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -7,5 +9,26 @@ export class ModelService {
 
 	constructor() { 
 
+	}
+
+	getPathsForCareerGoalQuestions(careerGoal, nextQuestions) {
+		let rtn = []
+		let id = nextQuestions[0]['id'];
+
+		jsonpath.nodes(careerGoal, "$..questions").forEach(e => {
+			let qIds = e['value'].map(q => q['id']);
+
+			let nextQuestionIds = nextQuestions.map(q => q['id']);
+			let found = false;
+
+			for (var i=0; i < qIds.length && !found; i++) {
+				if (nextQuestionIds.find(id => id === qIds[i])) {
+					rtn.push(e['path']);
+					found = true;
+				}
+			}
+		})
+
+		return rtn;
 	}
 }
